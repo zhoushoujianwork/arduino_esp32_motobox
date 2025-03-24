ab1fefc232ea8ab46652f3159f7bf0cc7e1bfbba
zhoushoujian <zhoushoujianwork@163.com>
2025-03-24 22:16:12 +0800

    feat: 更新platformio.ini和config.h文件，集中管理设备配置和模式定义，添加多种模式支持及相关参数

--- src/config.h
+++ src/config.h
@@ -6,109 +6,114 @@
 
 /*
  * Device Operation Mode Configuration
- * Supported modes:
+ * Modes are now defined in platformio.ini build_flags:
  * 1. MODE_ALLINONE: Standalone mode without Bluetooth, direct data collection and display
- * 2. SERVER: Master mode - collects sensor data, sends via Bluetooth to client,
+ * 2. MODE_SERVER: Master mode - collects sensor data, sends via Bluetooth to client,
  *           and can send data to server via MQTT over WiFi
- * 3. CLIENT: Slave mode - receives data from server via Bluetooth and displays it
+ * 3. MODE_CLIENT: Slave mode - receives data from server via Bluetooth and displays it
  */
-// #define MODE_ALLINONE
-#define MODE_SERVER
-// #define MODE_CLIENT
+
+/*
+ * 以下定义保留是为了兼容性，实际的值在 platformio.ini 中配置
+ * Common device configurations are now defined in platformio.ini [env] section
+ * Pin configurations are defined in respective environment sections [env:allinone], [env:server], [env:client]
+ */
 
 /* Common Device Configuration */
+#ifndef BLE_NAME
 #define BLE_NAME            "ESP32-MOTO"
+#endif
+
+#ifndef SERVICE_UUID
 #define SERVICE_UUID        "4FAFC201-1FB5-459E-8FCC-C5C9C331914B"
+#endif
+
+#ifndef DEVICE_CHAR_UUID
 #define DEVICE_CHAR_UUID    "BEB5483A-36E1-4688-B7F5-EA07361B26A8"
+#endif
+
+#ifndef GPS_CHAR_UUID
 #define GPS_CHAR_UUID       "BEB5483E-36E1-4688-B7F5-EA07361B26A8"
+#endif
+
+#ifndef IMU_CHAR_UUID
 #define IMU_CHAR_UUID       "BEB5483F-36E1-4688-B7F5-EA07361B26A8"
+#endif
 
 /* Battery Configuration */
+#ifndef BAT_MIN_VOLTAGE
 #define BAT_MIN_VOLTAGE     2900
+#endif
+
+#ifndef BAT_MAX_VOLTAGE
 #define BAT_MAX_VOLTAGE     3250
+#endif
 
 /* Display Configuration */
+#ifndef TFT_HOR_RES
 #define TFT_HOR_RES        172
+#endif
+
+#ifndef TFT_VER_RES
 #define TFT_VER_RES        320
+#endif
+
+#ifndef UI_MAX_SPEED
 #define UI_MAX_SPEED       199    /* Maximum speed in km/h */
+#endif
 
 /* LED Configuration */
+#ifndef LED_BLINK_INTERVAL
 #define LED_BLINK_INTERVAL 100
+#endif
 
 /* IMU Configuration */
+#ifndef IMU_MAX_D
 #define IMU_MAX_D          68     /* Marquis GP retention */
+#endif
 
 /* GPS Configuration */
+#ifndef GPS_DEFAULT_BAUDRATE
 #define GPS_DEFAULT_BAUDRATE 9600
+#endif
+
+#ifndef GPS_HZ
 #define GPS_HZ              2      /* Supported: 1, 2, 5, 10 Hz */
+#endif
 
 /* MQTT Configuration */
+#ifndef MQTT_SERVER
 #define MQTT_SERVER        "mq-hub.daboluo.cc"
-#define MQTT_PORT         1883
-#define MQTT_USER         ""
-#define MQTT_PASSWORD     ""
+#endif
 
-/* MQTT Topics */
+#ifndef MQTT_PORT
+#define MQTT_PORT         1883
+#endif
+
+#ifndef MQTT_USER
+#define MQTT_USER         ""
+#endif
+
+#ifndef MQTT_PASSWORD
+#define MQTT_PASSWORD     ""
+#endif
+
+/* MQTT Topics - These must stay in C++ code because they include runtime expressions */
 #define MQTT_TOPIC_DEVICE_INFO  String("vehicle/v1/") + device.get_device_id() + "/device/info"
 #define MQTT_TOPIC_GPS         String("vehicle/v1/") + device.get_device_id() + "/gps/position"
 #define MQTT_TOPIC_IMU         String("vehicle/v1/") + device.get_device_id() + "/imu/gyro"
 
 /* MQTT Intervals (in milliseconds) */
+#ifndef MQTT_DEVICE_INFO_INTERVAL
 #define MQTT_DEVICE_INFO_INTERVAL  5000
+#endif
+
+#ifndef MQTT_IMU_PUBLISH_INTERVAL
 #define MQTT_IMU_PUBLISH_INTERVAL   200
+#endif
+
+#ifndef MQTT_BAT_PRINT_INTERVAL
 #define MQTT_BAT_PRINT_INTERVAL   10000
-
-#ifdef MODE_ALLINONE
-    /* Pin Configuration for ALL_IN_ONE Mode */
-    #define BTN_PIN         39
-    #define BAT_PIN         20
-    #define IMU_SDA_PIN     42
-    #define IMU_SCL_PIN      2
-    #define LED_PIN          8
-
-    /* TFT Display Pins */
-    #define TFT_CS           6
-    #define TFT_DC           4
-    #define TFT_MOSI        15
-    #define TFT_SCLK         5
-    #define TFT_RST          7
-    #define TFT_BL          16
-
-    /* GPS UART Pins */
-    #define GPS_RX_PIN      12
-    #define GPS_TX_PIN      11
-#endif
-
-#ifdef MODE_SERVER
-    #define BLE_SERVER              /* Enable server mode */
-    
-    /* Pin Configuration for SERVER Mode */
-    #define IMU_SDA_PIN     42
-    #define IMU_SCL_PIN      2
-    #define BTN_PIN         39
-    #define BAT_PIN          7
-    #define LED_PIN          8
-    
-    /* GPS UART Pins */
-    #define GPS_RX_PIN      12
-    #define GPS_TX_PIN      11
-#endif
-
-#ifdef MODE_CLIENT
-    #define BLE_CLIENT              /* Enable client mode */
-    
-    /* Pin Configuration for CLIENT Mode */
-    #define BAT_PIN         14
-    #define LED_PIN          3
-
-    /* TFT Display Pins */
-    #define TFT_CS 15
-    #define TFT_DC 2
-    #define TFT_MOSI 19
-    #define TFT_SCLK 18
-    #define TFT_RST 4
-    // #define TFT_BL 5
-    // GPIO5 不支持 PWM 输出 调整亮度，16 是支持的
 #endif
 
 #endif /* CONFIG_H */
